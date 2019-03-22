96fa14d539d6cb2c3118750d289724ff7589f989
nwrobel <nickwrobel2@gmail.com>
2019-03-22 13:15:36 -0400

    finished the deploy-windows PS script to deploy the project to windows easily; updated the setup.py script to also activate the virtual environment before a script is run, so the user doesn't need to do it

--- scripts/setup.py
+++ scripts/setup.py
@@ -6,7 +6,12 @@ Created on Mar 17, 2019
 
 def PrepareScriptsForExecution():
     import sys, os
+    
+    # Add project root to PYTHONPATH so MLU modules can be imported
     scriptPath = os.path.dirname(os.path.realpath(__file__))
     projectRoot = os.path.abspath(os.path.join(scriptPath ,".."))
     sys.path.append(projectRoot)
     
+    # Activate the virtual environment, so you can import libraries in the virtualenv
+    activateThisFilepath = os.path.join(projectRoot, "py-venv/bin/activate_this.py")
+    exec(open(activateThisFilepath).read())
