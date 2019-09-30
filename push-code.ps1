50eed6a4efc6bd28780c39332c40e6062e213dc3
nwrobel <nickwrobel2@gmail.com>
2019-09-30 10:29:01 -0400

    Removed PS code script for playlist convertion, since Python version already exists and is more updated

--- push-code.ps1
+++ push-code.ps1
@@ -4,18 +4,20 @@ $ErrorActionPreference = 'Stop'
 $pyCacheDirs = Get-ChildItem -Filter "__pycache__" -Directory -Recurse
 $pyCacheDirs | Remove-Item -Force -Recurse -Confirm:$false
 
-$pycFiles = Get-ChildItem -Filter "*.pyc" -File -Recurse
-$pycFiles | Remove-Item -Force
+$pyCacheFiles = Get-ChildItem -Filter "*.pyc" -File -Recurse
+$pyCacheFiles | Remove-Item -Force
 
-Write-Host "The following cache files/folders were deleted:"
-$pycFiles | Select-Object -ExpandProperty FullName
-$pyCacheDirs | Select-Object -ExpandProperty FullName
+if ($pyCacheDirs -or $pyCacheFiles) {
+    Write-Host "The following Python cache files/folders were deleted (pre-commit cleanup procedure):"
+    $pycFiles | Select-Object -ExpandProperty FullName
+    $pyCacheDirs | Select-Object -ExpandProperty FullName
+}
 
-$commitMsg = Read-Host -Prompt "Enter commit message"
+$commitMsg = Read-Host -Prompt "`nEnter commit message"
 
 git add --all
 git commit -m $commitMsg
 git push origin
 
+Write-Host "Commit pushed successfully!" -ForegroundColor Green
 
-Write-Host "Cleared cache and pushed changes successfully!" -ForegroundColor Green
