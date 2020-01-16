3380d259074a763fc0a7d36d9e6f1ac317bbeebe
Nick Wrobel <nickwrobel2@gmail.com>
2020-01-16 12:52:44 -0500

    finished creating mlu.tags.base class for all tag handler classes, and finished the basic tags handler inferface class

--- mlu/tags/base.py
+++ mlu/tags/base.py
@@ -1,19 +1,42 @@
+'''
+mlu.tags.base
+
+First created: 01/15/20
+Last modified: 01/16/20
+
+This module contains the definition of the base, abstract class SongTagsHandler. This class serves
+as a blueprint and partial starting implementation for other SongTagsHandler-type classes in the 
+tags module to build off of.
+
+''' 
+
 import os
 from abc import ABC, abstractmethod
 
 class SongTagsHandler(ABC):
+    '''
+    Abstract base class that is a blueprint and partial starting implementation for the 
+    SongTagsHandler-type classes, which handle specific sets of audio tags, to be built from.
 
+    The blueprint requires that a tags handler class implement 4 public methods and 1 private method:
+        readTags: to read the tag values from the audio file
+        writeTags: to write the tag values to the audio file
+        getTags: to return the tag values of this file to the outside code
+        setTags: to validate and set the tag values to the given values
+        _validateTagValues: to validate the tags, throwing an exception if they are invalid (used
+            by setTags)
+
+    All child classes must accept the param 'songFilepath' as their single constructor argument, 
+    which is the path of the audio file that the tag handler represents. 
+    This is done by calling the super class (this class) and passing the songFilepath param to it.
+    This class will do validation for the songFilepath given.  
+    '''
     def __init__(self, songFilepath):
         # validate that the given is a valid actual filepath (does not check existence)
         if (not os.path.isabs(songFilepath)):
-            raise TypeError("Class field 'songFilepath' must be a valid absolute filepath string (either existent or non-existent)")
+            raise TypeError("Class attribute 'songFilepath' must be a valid absolute filepath string (either existent or non-existent)")
 
         self._songFilepath = songFilepath
-
-
-    @abstractmethod
-    def _validateTagValues(self):
-        pass  
 
     @abstractmethod
     def readTags(self):
@@ -29,4 +52,8 @@ class SongTagsHandler(ABC):
 
     @abstractmethod
     def setTags(self):
+        pass
+
+    @abstractmethod
+    def _validateTagValues(self):
         pass
--- mlu/tags/basic.py
+++ mlu/tags/basic.py
@@ -1,86 +1,172 @@
+'''
+mlu.tags.basic
+
+First created: ?
+Last modified: 01/16/20
+
+This module contains the SongBasicTagsHandler class, which handles the set of "basic" tags for
+a single audio file. It also contains the SongBasicTags class object, which is meant to hold
+the basic tag values of the audio file when they are returned via getTags().
+
+The following audio tags are handled by this tag handler, meaning they are read from and written
+to the file and can be gotten and set:
+- TITLE
+- ARTIST
+- ALBUM
+
+'''
+
+# TODO:
+# modify ratestat and playstat tags to have the same structure as this, creating the MLU
+# project's API for interacting with audio file tags for a single file
+#
+# Later we can also have classes to deal with handling collections of songs, which will use these
+# classes under the hood
+
 import mutagen
 
-# Returns the following tag values for the given audio file, given its filepath:
-#   durationSeconds
-#   title
-#   artist
-#   album
-#
+# setup logging for the module using preconfigured MLU logger
+import mlu.common.logger
+logger = mlu.common.logger.getMLULogger()
 
-class SingleSongBasicTagsHandler:
+from mlu.tags.base import SongTagsHandler
+
+class SongBasicTags:
     '''
-    Private class which handles the 'basic' tag values for a single audio file and allows
-    new, updated values to be set. This class also deals with tag value reading from and writing
-    to the audio file itself.
+    Simple container class that is used to pass around basic song tags to external code. The
+    SongBasicTagsHandler returns this object type when .getTags() is called.
+    '''
+    def __init__(self, songFilepath, title, artist, album, duration):
+        self.songFilepath = songFilepath
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.duration = duration
+
+class SongBasicTagsHandler(SongTagsHandler):
+    '''
+    Public class which handles the 'basic' tag values for a single audio file, allowing these tags
+    to be read from the file and for new, updated tag values to be set after being validated. 
+    This class also deals with tag value reading from and writing to the audio file itself.
     '''
     def __init__(self, songFilepath):
-        self.songFilepath = songFilepath
-        self.title = None 
-        self.artist = None 
-        self.album = None
-        self.durationSeconds = None
+        # base tag handler class enforces that songFilepath must be set: pass songFilepath to
+        # the base class to have it set there (and also here in the child class)
+        super().__init__(songFilepath)
 
-        # Throw exceptions is the wrong data type is passed in
-        if (not isinstance(self.songFilepath, str)):
-            raise TypeError("Constructor parameter 'songFilepath' must be a string")
+        self._title = None 
+        self._artist = None 
+        self._album = None
+        self._duration = None
 
-        # read in the tag values to populate the fields
-        self._readBasicTags()
+        self.readTags()
 
-    def _readBasicTags(self):
-        '''
+    def readTags(self):
+       '''
         Reads in the current values of all basic tags, updating the class instance's fields with
         this data.
         '''
-        audioFile = mutagen.File(self.songFilepath)
+        audioFile = mutagen.File(self._songFilepath)
 
+        # set the fields from the raw tag values and properties
         try:
             rawTitleTag = audioFile['TITLE']
             rawTitleTag = rawTitleTag[0]
+            self._title = rawTitleTag
+
         except KeyError:
-            rawTitleTag = ''
+            logger.debug("No tag value found for tag name 'TITLE' for song '{}': leaving instance variable Null".format(self._songFilepath))
 
         try:
             rawArtistTag = audioFile['ARTIST']
             rawArtistTag = rawArtistTag[0]
+            self._artist = rawArtistTag
+
         except KeyError:
-            rawArtistTag = ''
+            logger.debug("No tag value found for tag name 'ARTIST' for song '{}': leaving instance variable Null".format(self._songFilepath))
 
         try:
             rawAlbumTag = audioFile['ALBUM']
             rawAlbumTag = rawAlbumTag[0]
+            self._album = rawAlbumTag
+
         except KeyError:
-            rawAlbumTag = ''
+            logger.debug("No tag value found for tag name 'ALBUM' for song '{}': leaving instance variable Null".format(self._songFilepath))
 
-        try:
-            durationProperty = audioFile.info.length
-        except:
-            logger.exception("Unable to set basic tag 'duration' ")
+        # audio file should always have a duration
+        durationProperty = audioFile.info.length
+        self._duration = durationProperty
 
-        self.title = rawTitleTag
-        self.artist = rawArtistTag
-        self.album = rawAlbumTag
-
-        logger.debug("Successfully READ basic tags for audio file: Path='{}', Title='{}', Artist='{}', Album='{}'".format(self.songFilepath, self.title, self.artist, self.album))
+        logger.debug("Successfully READ basic tags for audio file: Path='{}', Title='{}', Artist='{}', Album='{}', Duration='{}'".format(self._songFilepath, self._title, self._artist, self._album, self._duration))
 
 
-def getSongBasicTags(songFilepath):
-    """
-    Retrieves the tag values of the given audio filepath for several standard, common tags. Returns
-    a SongBasicTags object, containing the audio's values for tags: title, artist, album, duration.
+    def writeTags(self):
+        '''
+        Writes the tags that are currently set (saved in this SongBasicTagsHandler instance) to the
+        audio file, replacing the file's old basic tags with the current (new) values.
+        '''
+        audioFile = mutagen.File(self._songFilepath)
 
-    Args:
-        songFilepath: the absolute filepath of the song/audio file
+        # Write all basic tags, except for 'duration', which is an immutable property of the file
+        audioFile['TITLE'] = self._title
+        audioFile['ARTIST'] = self._artist
+        audioFile['ALBUM'] = self._album
 
-    Returns:
-        SongBasicTags object holding the basic tags' values for the song
-    """
-    audioFile = mutagen.File(songFilepath)
-    tags = SongBasicTags(
-        title=audioFile['TITLE'],
-        artist=audioFile['ARTIST'],
-        album=audioFile['ALBUM'],
-        durationSeconds=audioFile.info.length
-    )
+        audioFile.save()
+        logger.debug("Successfully WROTE basic tags for audio file: Path='{}', Title='{}', Artist='{}', Album='{}'".format(self._songFilepath, self._title, self._artist, self._album))
 
-    return tags
+
+    def getTags(self):
+        '''
+        Returns a SongBasicTags object, which holds the song filepath as well as the current basic 
+        tag values for the song represented by this SongBasicTagsHandler instance.
+        '''
+        return SongBasicTags(
+            songFilepath=self._songFilepath
+            title=self._title,
+            artist=self._artist,
+            album=self._album,
+            duration=self._duration
+        )
+
+
+    def setTags(self, title, artist, album):
+        '''
+        Allows the tag values saved in this SongBasicTagsHandler instance to be changed/set.
+        Validation on the given values for each tag is performed and an exception will be thrown
+        if any tag values fail this validation checking.
+
+        Params:
+            title: the new desired song title tag value (must be set)
+            artist: the new desired song artist tag value (must be set)
+            album: the new desired song album tag value (can be empty/unset, pass the empty string
+                as the value if so)
+        '''
+        # check for validity of input values and then set the tags to the given values 
+        # this will throw an exception if any of these given tag values are invalid
+        self._validateTagValues(title, artist, album)
+
+        self._title = title
+        self._artist = artist
+
+        if (album):
+            self._album = album
+        else:
+            self._album = None
+
+        logger.debug("Successfully SET tag values for SongBasicTagHander instance: Path='{}', Title='{}', Artist='{}', Album='{}'".format(self._songFilepath, self._title, self._artist, self._album))
+
+
+    def _validateTagValues(self, title, artist, album):
+        '''
+        Private method that performs validation of the given tag values, ensuring that an invalid
+        value is not set.
+        '''
+        if (not title) or (not isinstance(title, str)):
+            raise TypeError("Class attribute 'title' must be a non-empty string")
+
+        if (not title) or (not isinstance(artist, str)):
+            raise TypeError("Class attribute 'artist' must be a non-empty string")
+
+        if (not isinstance(album, str)):
+            raise TypeError("Class attribute 'album' must be a string (the empty string is also valid if no album tag exists)")
