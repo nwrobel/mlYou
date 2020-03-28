18958028314548cc56f0770bc5fb5907ba83d0fe
Nick Wrobel <nickwrobel2@gmail.com>
2020-03-28 18:30:46 -0400

    finished tags.io module, writing unit tests for it

--- mlu/tags/io.py
+++ mlu/tags/io.py
@@ -15,53 +15,97 @@ class AudioFileTagIOHandler:
     def __init__(self, audioFilepath):
         # validate that the given is a valid possible filepath
         if (not mlu.common.file.isValidPossibleFilepath(audioFilepath)):
-            raise TypeError("Class attribute 'audioFilepath' must be a valid absolute filepath string (either existent or non-existent)")
+            raise ValueError("Class attribute 'audioFilepath' must be a valid absolute filepath string to an existing file")
 
         self.audioFilepath = audioFilepath
-        self.mutagenMp3Interface = None
-        self.mutagenFlacInterface = None
+        audioFileExt = mlu.common.file.GetFileExtension(self.audioFilepath)
 
-        # audioFileExt = mlu.common.file.GetFileExtension(self.audioFilepath)
+        if (audioFileExt == '.flac'):
+            self.audioFileType = 'flac'
+        elif (audioFileExt == '.mp3'):
+            self.audioFileType = 'mp3'
+        else:
+            raise Exception("Cannot open file '{}': Audio file format is not supported".format(self.audioFilepath))
 
-        # if (audioFileExt == '.flac'):
-        #     self.mutagenFlacInterface = mutagen.File(self.audioFilepath)
-        # elif (audioFileExt == '.mp3'):
-        #     self.mutagenMp3Interface = ID3(self.audioFilepath, v2_version=3)
-        # else:
-        #     raise Exception("Cannot open file '{}': Audio file format is not supported".format(self.audioFilepath))
 
     def getAudioTagValue(self, tagName):
+        if (self.audioFileType == 'flac'):
+            tagValue = self._getAudioTagValueFromFLACFile(tagName)
 
-        # if (self.mutagenFlacInterface):
-        #     mutagenInterface = self.mutagenFlacInterface
-        # elif (self.mutagenMp3Interface):
-        #     mutagenInterface = self.mutagenMp3Interface
-        # else:
-        #     raise Exception("No mutagen tag interface instance was found")
+        elif (self.audioFileType == 'mp3'):
+            tagValue = self._getAudioTagValueFromMp3File(tagName)
 
-        # try:
-        #     tagValue = mutagenInterface[tagName][0]
-        # except KeyError:
-        #     tagValue = ''
-
-        keys = mutagen.File(self.audioFilepath).keys()
-        mutagen.File(self.audioFilepath)['TXXX:play_count'].text
-
-        return keys
+        return tagValue
 
 
     def setAudioTagValue(self, tagName, newValue):
+        if (self.audioFileType == 'flac'):
+            self._setAudioTagValueForFLACFile(tagName, newValue)
 
-        if (self.mutagenFlacInterface):
-            mutagenInterface = self.mutagenFlacInterface
-        elif (self.mutagenMp3Interface):
-            mutagenInterface = self.mutagenMp3Interface
+        elif (self.audioFileType == 'mp3'):
+            self._setAudioTagValueForMp3File(tagName, newValue)
+
+
+    def _getAudioTagValueFromFLACFile(self, tagName):
+        tagName = tagName.upper()
+        mutagenInterface = mutagen.File(self.audioFilepath)
+        try:
+            tagValue = mutagenInterface[tagName][0]
+        except KeyError:
+            tagValue = ''
+
+        return tagValue
+
+
+    def _setAudioTagValueForFLACFile(self, tagName, newValue):
+        tagName = tagName.upper()
+        mutagenInterface = mutagen.File(self.audioFilepath)
+
+        mutagenInterface[tagName] = newValue
+        mutagenInterface.save()
+
+
+    def _getAudioTagValueFromMp3File(self, tagName):
+
+        tagName = tagName.upper()
+        tagValue = ''
+
+        standardId3Tags = [tag.upper() for tag in EasyID3.valid_keys.keys()]
+        if (tagName in standardId3Tags):
+
+            mutagenInterface = EasyID3(self.audioFilepath)
+
+            try:
+                tagValue = mutagenInterface[tagName][0]
+            except KeyError:
+                print('Tag not set')
+
         else:
-            raise Exception("No mutagen tag interface instance was found")
+            mutagenInterface = mutagen.File(self.audioFilepath)
+            id3TagName = "TXXX:{}".format(tagName)
 
+            try:
+                tagValue = mutagenInterface[id3TagName].text[0]
+            except KeyError:
+                print('Tag not set')
+
+        return tagValue
+
+
+    def _setAudioTagValueForMp3File(self, tagName, newValue):
+
+        tagName = tagName.upper()
         newValueStr = str(newValue)
-        mutagenInterface.add(TXXX(3, desc=tagName ,text=newValueStr))
-        mutagenInterface.save(self.audioFilepath, v2_version=3)
-    
-        # self.mutagenTagInterface[tagName] = newValue 
-        # self.mutagenTagInterface.save()
+
+        standardId3Tags = [tag.upper() for tag in EasyID3.valid_keys.keys()]
+        if (tagName in standardId3Tags):
+
+            mutagenInterface = EasyID3(self.audioFilepath)
+            mutagenInterface[tagName] = newValueStr
+            mutagenInterface.save()
+
+        else:
+            mutagenInterface = ID3(self.audioFilepath, v2_version=3)
+
+            mutagenInterface.add(TXXX(3, desc=tagName, text=newValueStr))
+            mutagenInterface.save(self.audioFilepath, v2_version=3)
---
+++ mlutest/tags_test.py
@@ -0,0 +1,57 @@
+import unittest
+
+# Do setup processing so that this script can import all the needed modules from the "mlu" package.
+# This is necessary because these scripts are not located in the root directory of the project, but
+# instead in the 'scripts' folder.
+import envsetup
+envsetup.PreparePythonProjectEnvironment()
+
+import mlu.tags.io
+
+class TestTagsIOModule(unittest.TestCase):
+    def setUp(self):
+        self.testAudioFilepathFLAC = "D:\\Temp\mlu-test\\test-music-lib\\Content\Music\\(hed) Planet Earth\\The Best Of (Hed) Planet Earth\\1.01. (hed) Planet Earth - Suck It Up.flac"
+        self.testAudioFilepathMp3 = "D:\\Temp\mlu-test\\test-music-lib\\Content\Music\\Derek Trucks And Co\\The Derek Trucks Band\\1997 - The Derek Trucks Band (320 kbps)\\01 - Sarod.mp3"
+        self.testAudioFileALAC = "D:\\Temp\\mlu-test\\test-music-lib\\Content\\Music\\Buckethead [ALAC]\\Studio albums\\[1992]Bucketheadland\\CD1\\01. Buckethead - Intro- Park Theme.m4a"
+        self.notExistFile = 'D:\\hello.mp3'
+
+    def testAudioFileTagIOHandlerConstructor(self):
+        # Test nonexisting file given
+        self.assertRaises(ValueError, mlu.tags.io.AudioFileTagIOHandler, self.notExistFile)
+
+        # Test FLAC file given
+        handler = mlu.tags.io.AudioFileTagIOHandler(self.testAudioFilepathFLAC)
+        self.assertEqual(handler.audioFileType, 'flac')
+
+        # Test MP3 file given
+        handler = mlu.tags.io.AudioFileTagIOHandler(self.testAudioFilepathMp3)
+        self.assertEqual(handler.audioFileType, 'mp3')
+
+        # Test non-supported filetype given
+        self.assertRaises(ValueError, mlu.tags.io.AudioFileTagIOHandler, self.testAudioFileALAC)
+
+
+    def testAudioFileTagIOHandlerFLAC(self):
+        # Test tag reading: defined tag
+        handler = mlu.tags.io.AudioFileTagIOHandler(self.testAudioFilepathFLAC)
+        actualValue = handler.getAudioTagValue('title')
+        expectedValue = 'Intro- Park Theme'
+        self.assertEqual(actualValue, expectedValue)
+
+        # Test tag reading: undefined tag
+        actualValue = handler.getAudioTagValue('undefined_tag')
+        self.assertEqual(actualValue, '')
+
+        # Test tag writing
+        handler = mlu.tags.io.AudioFileTagIOHandler(self.testAudioFilepathFLAC)
+        testValue = 'asdf'
+        handler.setAudioTagValue('tag_name', testValue)
+        
+        actualValue = handler.getAudioTagValue('tag_name')
+        self.assertEqual(actualValue, testValue)
+
+    def testAudioFileTagIOHandlerMp3(self):
+        tag1Start = handler.getAudioTagValue('title')
+        tag2Start = handler.getAudioTagValue('copyright')
+        tag3Start = handler.getAudioTagValue('play_count')
+        tag4Start = handler.getAudioTagValue('undefined_tag')
