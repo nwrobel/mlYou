f4646f8b553e4abf3358fbeb824cf2c16dda26d4
Nick Wrobel <nickwrobel2@gmail.com>
2020-05-06 15:25:27 -0400

    updated ratestats module to use new design and use the new tags.io module; updated vote playlists update script to use the new ratestats module

--- mlu/tags/ratestats.py
+++ mlu/tags/ratestats.py
@@ -4,193 +4,104 @@ mlu.tags.ratestats
 First created: ?
 Last modified: 01/17/20
 
-Module containing functionality for the music rating system of the music library. Handles reading 
-and setting the following tags for audio files:
-- VOTES
-- RATING
-- NEEDS_RATING_UPDATE
+Module containing functionality for the music rating system of the music library.
 
 '''
-
-# TODO: 
-# Test to see what happens when an audio file has no tag of name set at all: Does this make a KeyError,
-# or will it be a null value?
-
-import mutagen
-import mutagen.flac
 
 # setup logging for the module using preconfigured MLU logger
 import mlu.common.logger
 logger = mlu.common.logger.getMLULogger()
 
-from mlu.tags.base import SongTagsHandler
+import mlu.tags.io
 import mlu.tags.common
+import mlu.common.file 
+import mlu.common.time
+import mlu.library.playlist
 
-class SongRatestatTags:
-    '''
-    Simple container class that is used to pass around ratestat song tags to external code. The
-    SongRatestatTagsHandler returns this object type when .getTags() is called.
-    '''
-    def __init__(self, songFilepath, votes, rating, needsRatingUpdate):
-        self.songFilepath = songFilepath
+class AudioFileVoteData:
+    def __init__(self, filepath, votes):
+        self.filepath = filepath
         self.votes = votes
-        self.rating = rating
-        self.needsRatingUpdate = needsRatingUpdate
 
-
-class SongRatestatTagsHandler(SongTagsHandler):
+def updateRatestatTags(audioFileVoteData):
     '''
-    Public class which handles the 'ratestat' tag values for a single audio file and allows
-    new, updated values to be calculated. This class also deals with tag value reading from and writing
-    to the audio file itself.
     '''
-    def __init__(self, songFilepath):
-        # base tag handler class enforces that songFilepath must be set: pass songFilepath to
-        # the base class to have it set there (and also here in the child class)
-        super().__init__(songFilepath)
+    logger.debug("Updating ratestat tags with new votes: File={}, NewVotes={}".format(audioFileVoteData.filepath, audioFileVoteData.votes))
 
-        self._votes = None # list of numbers (int), each representing a vote
-        self._rating = None # single float number for the avg vote value
-        self._needsRatingUpdate = None # bool as to whether or not this audio file needs its rating tag updated
+    tagHandler = mlu.tags.io.AudioFileTagIOHandler(audioFileVoteData.filepath)
+    currentTags = tagHandler.getTags()
 
-        # read in the tag values to populate the fields
-        self.readTags()
+    votesCurrent = mlu.tags.common.formatAudioTagToValuesList(currentTags.votes, valuesAsInt=True) 
+    votesUpdated = votesCurrent + audioFileVoteData.votes
+    votesUpdatedTagValue = mlu.tags.common.formatValuesListToAudioTag(votesUpdated)
 
+    newTags = currentTags
+    newTags.votes = votesUpdatedTagValue
+    newTags.rating = _calculateRatingTagValue(votesUpdated)
+    tagHandler.setTags(newTags)
 
-    def readTags(self):
-        '''
-        Reads in the current values of all ratestat tags, updating the class instance's fields with
-        this data.
-        '''
-        audioFile = mutagen.File(self._songFilepath)
-
-        # Read VOTES tag
-        try:
-            rawVotesTag = audioFile['VOTES'][0]
-        except KeyError:
-            rawVotesTag = ''
-
-        if (not rawVotesTag):
-            logger.debug("Tag value 'VOTES' is empty for song '{}': tag handler will use value '[]'".format(self._songFilepath))
-            self._votes = []
-        else:
-            self._votes = mlu.tags.common.formatAudioTagToValuesList(rawVotesTag, valuesAsInt=True)
-        
-        # Read RATING tag
-        try:
-            rawRatingTag = audioFile['RATING'][0]
-        except KeyError:
-            rawRatingTag = ''
-
-        if (not rawRatingTag):
-            logger.debug("Tag value 'RATING' is empty for song '{}': tag handler will use value '0'".format(self._songFilepath))
-            self._rating = 0
-        else:
-            self._rating = float(rawRatingTag)
-
-        # Read NEEDS_RATING_UPDATE tag
-        try:
-            rawNeedRatingUpdateTag = audioFile['NEEDS_RATING_UPDATE'][0]
-        except KeyError:
-            rawNeedRatingUpdateTag = ''
-
-        if (not rawNeedRatingUpdateTag):
-            logger.debug("Tag value 'NEEDS_RATING_UPDATE' is empty for song '{}': tag handler will use value 'False'".format(self._songFilepath))
-            self._needsRatingUpdate = False
-        else:
-            self._needsRatingUpdate = (int(rawNeedRatingUpdateTag) == 1)
-
-        logger.debug("Successfully READ ratestat tags for audio file: Path={}, Votes={}, Rating={}, NeedsRatingUpdate={}".format(self._songFilepath, self._votes, self._rating, self._needsRatingUpdate))
+    logger.info("Updated ratestat tags to the following values: File={}, Votes={}, Rating={}".format(audioFileVoteData.filepath, newTags.votes, newTags.rating))
 
 
-    def writeTags(self):
+def getAudioFileVoteDataFromRatePlaylists(playlistsDir):
+    '''
+    '''
+    audioFileVoteDataList = []
 
-        if (not self._tagsHaveBeenSet):
-            raise Exception("Cannot write ratestat tags: tag values have not yet been set for audio file '{}': call setTags() first before writing new tag values".format(self._songFilepath))
+    for currentVoteValue in range(1, 10):
+        currentVoteValuePlaylistName = "{}.m3u8".format(currentVoteValue)
+        currentVoteValuePlaylistPath =  mlu.common.file.JoinPaths(playlistsDir, currentVoteValuePlaylistName)
 
-        audioFile = mutagen.File(self._songFilepath)
+        logger.info("Reading songs with vote value {} from playlist {}".format(currentVoteValue, currentVoteValuePlaylistPath))
+        playlistSongs = mlu.library.playlist.getAllPlaylistLines(currentVoteValuePlaylistPath)
+        logger.info("Found {} songs in vote value {} playlist: loading them into the data structure".format(len(playlistSongs), currentVoteValue))
 
-        # Write VOTES tag
-        # Form the tag value string (seperated by ;) from the votes list
-        rawVotesTag = mlu.tags.common.formatValuesListToAudioTag(self._votes)
-        audioFile['VOTES'] = rawVotesTag
+        for songFilepath in playlistSongs:
+            logger.debug("Adding new vote (value {}) for song '{}'".format(currentVoteValue, songFilepath))
 
-        # Write the RATING tag
-        # round rating float value to 2 decimal points and always show 2 decimal places
-        rawRatingTag = '{0:.2f}'.format(round(self._rating, 2)) 
-        audioFile['RATING'] = rawRatingTag
-
-        # Write the NEEDS_RATING_UPDATE tag
-        # NeedsRatingUpdate flag should not be set: if it is, tags need to be updated (exception)
-        # If it's not set, good: the tag value for this flag will be 0
-        if (self._needsRatingUpdate):
-            raise Exception("Cannot write ratestat tags to audio file: 'needsRatingUpdate' instance variable is true despite tags having been set")
-        else:
-            rawNeedRatingUpdateTag = '0'
-        audioFile['NEEDS_RATING_UPDATE'] = rawNeedRatingUpdateTag
-
-        audioFile.save()
-        logger.debug("Successfully WROTE ratestat tags for audio file: Path={}, Votes={}, Rating={}, NeedsRatingUpdate={}".format(self._songFilepath, self._votes, self._rating, self._needsRatingUpdate))
-
-
-    def getTags(self):
-        '''
-        Returns a SongRatestatTags object, which holds the song filepath as well as the current basic 
-        tag values for the song represented by this SongBasicTagsHandler instance.
-        '''
-        return SongRatestatTags(
-            songFilepath=self._songFilepath,
-            votes=self._votes,
-            rating=self._rating,
-            needsRatingUpdate=self._needsRatingUpdate
-        )
-
-
-    def updateTags(self, newVote=None):
-        '''
-        Allows the ratestat tags to be updated for this song. This only updates the current values
-        saved in this handler instance: the changes are not written to the file until calling writeTags().
-
-        The only changes allowed to the public through this method are the addition of a vote value.
-        Validation of the given vote performed and an exception will be thrown if it fails this 
-        validation checking.
-
-        Calling updateTags() with a newVote value will add that vote to the votes tag, update the 
-        rating tag with the newly computed avg. vote value, and set the needsRatingUpdate tag to 0.
-
-        Calling updateTags() alone with no vote value will do the same, except no vote will be added
-        to the votes tag. Rating and needsRatingUpdate tag will still be updated, however.
- 
-        Params:
-            newVote: (int) the new vote to add to the ratestat tags - omit this parameter to update
-                tags only without adding a new vote
-        '''
-        if (newVote is not None):
-            self._updateVotes(newVote)
+            # Find the current song in the existing vote data, if it's there, and add the vote
+            # to the existing vote data object
+            currentSongVoteData = [voteData for voteData in audioFileVoteDataList if (voteData.filepath == songFilepath)]
             
-        self._updateRating()
+            if (currentSongVoteData):
+                currentSongVoteData = currentSongVoteData[0]
+                currentSongVoteData.votes.append(currentVoteValue)
+            
+            # If there's not an object made for this song, create one with the vote data and 
+            # add it to the vote data list
+            else:
+                currentSongVoteData = AudioFileVoteData(filepath=songFilepath, votes=[currentVoteValue])
+                audioFileVoteDataList.append(currentSongVoteData)
 
-        self._tagsHaveBeenSet = True
-        logger.debug("Successfully UPDATED tag values for SongRatestatTagsHandler instance: Path={}, Votes={}, Rating={}, NeedsRatingUpdate={}".format(self._songFilepath, self._votes, self._rating, self._needsRatingUpdate))
+    allVotedSongsCount = len(audioFileVoteDataList)
+    logger.info("Vote data loaded from playlists: found {} unique songs that were voted on".format(allVotedSongsCount))
 
+    return audioFileVoteDataList
 
-    def _updateVotes(self, newVote):
-        self._validateTagValues(newVote)
-        self._votes.append(newVote)
+# TODO: finish
+def archiveRatePlaylists(playlistsDir, archiveDir):
+    '''
+    '''
+    archiveFilename = "[{}] Archived vote playlists (already written to ratestat tags).gz".format(mlu.common.time.getCurrentFormattedTime())
+    archiveFilePath = mlu.common.file.JoinPaths(args.votePlaylistsArchiveDir, archiveFilename)
 
-    def _updateRating(self):
-        if (self._votes):
-            newAvgRating = sum(self._votes) / len(self._votes)
-        else:
-            newAvgRating = 0
-        
-        self._rating = newAvgRating
-        self._needsRatingUpdate = False
+    mlu.common.file.compressFileToArchive(inputFilePath=votePlaylists, archiveOutFilePath=archiveFilePath)
+    logger.info('Vote playlists successfully compressed into archive file {}'.format(archiveFilePath))
 
+# TODO: finish
+def resetRatePlaylists(playlistsDir):
+    '''
+    '''
+    for votePlaylist in votePlaylists:
+        mlu.common.file.clearFileContents(filePath=votePlaylist)
 
-    def _validateTagValues(self, newVote):
-        if (not isinstance(newVote, int)):
-            raise TypeError("Each vote item in class attribute 'votes' must be type integer")
+def _calculateRatingTagValue(votes):
+    if (votes):
+        rating = sum(self._votes) / len(self._votes)
+        rating = float(rating)
+        ratingTagValue = '{0:.2f}'.format(round(rating, 2))
 
-        if (newVote < 1 or newVote > 10):
-            raise ValueError("Each vote item in class attribute 'votes' must be an integer value ranging from 1 to 10 (inclusive)")
+    else:
+        ratingTagValue = ''
+
+    return ratingTagValue
--- scripts/update_ratestat_tags_from_vote_playlists.py
+++ scripts/update_ratestat_tags_from_vote_playlists.py
@@ -24,115 +24,57 @@ logger = mlu.common.logger.getMLULogger()
 
 # import project-related modules
 import mlu.tags.ratestats
-import mlu.common.file 
-import mlu.common.time
-import mlu.library.playlist
-import mlu.tags.basic
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("votePlaylistsInputDir", 
+parser.add_argument("playlistRootDir", 
                     help="absolute filepath of the folder containing the vote playlist files (1.m3u, 2.m3u, ..., 10.m3u) to use as the data source",
                     type=str)
 
-parser.add_argument("votePlaylistsArchiveDir", 
+parser.add_argument("playlistArchiveDir", 
                     help="absolute filepath of the folder where the archive of the vote playlist files should be saved for backup after the music rating tags are updated",
                     type=str)
 
 args = parser.parse_args()
-votePlaylists = []
-
-# Do a pre scan through all playlists to get a list of all the unique songs (filepaths) that will be affected
-# this will allow us to initialize our newVotes data structure to keep track of all the new votes 
-# that were added to each affected song
-allEntries = []
-logger.info("Performing initial pre-scan of songs in all vote-value playlists")
-
-for currentVoteValue in range(1, 10):
-    currentVoteValuePlaylistName = "{}.m3u8".format(currentVoteValue)
-    currentVoteValuePlaylistPath =  mlu.common.file.JoinPaths(args.votePlaylistsInputDir, currentVoteValuePlaylistName)
-    votePlaylists.append(currentVoteValuePlaylistPath)
-    
-    playlistSongs = mlu.library.playlist.getAllPlaylistLines(currentVoteValuePlaylistPath)    
-    allEntries += playlistSongs
-
-allVotedSongs = set(allEntries)
-logger.info("Pre-scan complete: found {} unique songs that were voted on".format(len(allVotedSongs)))
-
-# initialize the newVotes data structure: add a dictionary for each unique song to this list of data
-# dictionaries and set its new vote list to empty
-logger.info("Initializing data structure to hold the new votes for all songs")
-newVotesData = {}
-for songFilepath in allVotedSongs:
-    newVotesData[songFilepath] = []
 
 # Start the main ratestat tag-writing process
-logger.info("Populating data structure with the data from each vote playlist")
-
-for currentVoteValue in range(1, 10):
-    currentVoteValuePlaylistName = "{}.m3u8".format(currentVoteValue)
-    currentVoteValuePlaylistPath =  mlu.common.file.JoinPaths(args.votePlaylistsInputDir, currentVoteValuePlaylistName)
-    
-    logger.info("Reading songs with vote value {} from playlist {}".format(currentVoteValue, currentVoteValuePlaylistPath))
-    playlistSongs = mlu.library.playlist.getAllPlaylistLines(currentVoteValuePlaylistPath)    
-    logger.info("Found {} songs in vote value {} playlist: loading them into the data structure".format(len(playlistSongs), currentVoteValue))
-
-    for songFilepath in playlistSongs:
-        logger.debug("Modifying data structure: Adding new vote (value {}) for song '{}'".format(currentVoteValue, songFilepath))
-        newVotesData[songFilepath].append(currentVoteValue)
+logger.info("Loading audio file votes data from all vote playlists")
+audioFileVoteDataList = mlu.tags.ratestats.getAudioFileVoteDataFromRatePlaylists(args.playlistRootDir)
 
 logger.info("New votes data structure filled from vote playlists data successfully")
 logger.info("Writing tag data from data structure to audio files")
 
-# Sort the dictionary of new vote values according to the song filepath
-# newVotesData = sorted(newVotesData)
-
-for songFilepath, newVotes in newVotesData.items():
+for audioFileVoteData in audioFileVoteDataList:
     logger.debug("Processing new votes for audio file '{}'".format(songFilepath))
-    songRatestatTagsHandler = mlu.tags.ratestats.SongRatestatTagsHandler(songFilepath)
+    mlu.tags.ratestats.updateRatestatTags(audioFileVoteData)
 
-    for voteValue in newVotes:
-        logger.debug("Adding new vote (value {}) to votes list tag for audio file".format(voteValue))
-        songRatestatTagsHandler.updateTags(newVote=voteValue)
-
-    songRatestatTagsHandler.writeTags()
-    logger.debug("New votes saved into votes tag successfully for this file!")
-
-
-logger.info('Music vote/rating tag data update completed successfully: {} songs were updated'.format(len(allVotedSongs)))
+logger.info('Music ratestats tag data update completed successfully: {} songs were updated'.format(len(audioFileVoteDataList)))
 
 # Print the results of all updated songs in table form and what changes occurred
 tagUpdatesTable = PrettyTable()
-tagUpdatesTable.field_names = ["Artist", "Title", "Added Votes", "New Rating", "New Votes List"]
+tagUpdatesTable.field_names = ["Title", "Artist", "Votes Added", "New Rating", "New Votes List"]
 
-for songFilepath, newVotes in newVotesData.items():
+for audioFileVotesData in audioFileVoteDataList:
 
-    # Get the basic tags and the new ratestat tags for each song so we can use them in the table
-    songBasicTagsHandler = mlu.tags.basic.SongBasicTagsHandler(songFilepath)
-    songRatestatTagsHandler = mlu.tags.ratestats.SongRatestatTagsHandler(songFilepath)
+    tagHandler = mlu.tags.io.AudioFileTagIOHandler(audioFileVotesData.filepath)
+    currentTags = tagHandler.getTags()
 
-    songBasicTags = songBasicTagsHandler.getTags()
-    songRatestatTags = songRatestatTagsHandler.getTags()
+    votesAdded = mlu.tags.common.formatValuesListToAudioTag(audioFileVotesData.votes)
 
     tagUpdatesTable.add_row([
-        songBasicTags.artist, 
-        songBasicTags.title, 
-        newVotes,
-        songRatestatTags.rating,
-        songRatestatTags.votes
+        currentTags.title, 
+        currentTags.artist, 
+        votesAdded,
+        currentTags.rating,
+        currentTags.votes
     ])
 
 logger.info("\nThe following changes were made to music library:\n{}".format(tagUpdatesTable))
 
 logger.info('Archiving vote playlists...')
-archiveFilename = "[{}] Archived vote playlists (already written to ratestat tags).gz".format(mlu.common.time.getCurrentFormattedTime())
-archiveFilePath = mlu.common.file.JoinPaths(args.votePlaylistsArchiveDir, archiveFilename)
-
-mlu.common.file.compressFileToArchive(inputFilePath=votePlaylists, archiveOutFilePath=archiveFilePath)
-logger.info('Vote playlists successfully compressed into archive file {}'.format(archiveFilePath))
+mlu.tags.ratestats.archiveRatePlaylists(playlistsDir=args.playlistRootDir, archiveDir=args.playlistArchiveDir)
 
 logger.info('Emptying already counted votes from vote playlists...')
-for votePlaylist in votePlaylists:
-    mlu.common.file.clearFileContents(filePath=votePlaylist)
+mlu.tags.ratestats.resetRatePlaylists(playlistsDir=args.playlistRootDir)
 
-logger.info('Vote playlists reset successfully, script complete!')
+logger.info('Script complete')
